92587a57a040de5df4da1a46bb54f362dfa658db
mendelcohen <mmhakohen@gmail.com>
2022-02-11 11:03:56 -0500

    solves codility lowestNumber

--- practice.js
+++ practice.js
@@ -1382,4 +1382,37 @@ function longestWord(text) {
   }
   return longestWord;
 }
-console.log(longestWord("ABCd"));
+// console.log(longestWord("ABCd"));
+
+// Write a function:
+
+// function solution(A);
+
+// that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+
+// For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+
+// Given A = [1, 2, 3], the function should return 4.
+
+// Given A = [−1, −3], the function should return 1.
+
+// Write an efficient algorithm for the following assumptions:
+
+// N is an integer within the range [1..100,000];
+// each element of array A is an integer within the range [−1,000,000..1,000,000].
+function lowestNumber(A) {
+  var number = 1;
+  var array = A.filter(num => num > 0).sort();
+  if (1 < Math.min(array) || array.length === 0) {
+    return number;
+  } else {
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] !== array[i + 1] && array[i] + 1 !== array[i + 1]) {
+        return number = array[i] + 1;
+      }
+    }
+  }
+}
+console.log(lowestNumber([1, 3, 6, 4, 1, 2]));
+console.log(lowestNumber([1, 2, 3]));
+console.log(lowestNumber([-1, -3]));
